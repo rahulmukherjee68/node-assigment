02f9ae2a39e313d71bce419170995d514867e5d6
Rahul <rahulmukherjee68@gmail.com>
2020-03-19 01:46:53 +0530

    second commit from home

--- routes/user.js
+++ routes/user.js
@@ -5,33 +5,6 @@ const body = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
-// MongoClient.connect(url, function (err, db) {
-//     if (err) throw err;
-//     var dbo = db.db("mydb");
-//     dbo.createCollection("customers", function (err, res) {
-//         if (err) throw err;
-//         console.log("Collection created!");
-//         db.close();
-//     });
-// });
-
-// MongoClient.connect(url, function (err, db) {        
-//     if (err) throw err;
-//     var dbo = db.db("user");
-//     // var myobj = { name: "Company Inc", address: "Highway 37" };
-//     dbo.createcollection("user").insertOne(myobj, function (err, res) {
-//         if (err) {
-
-//         }
-//         else {
-
-//         }
-//         console.log("1 document inserted");
-//         db.close();
-//     });
-// });
-
-
 
 router.post('/', (req, res, next) => {
     var myobj = {
